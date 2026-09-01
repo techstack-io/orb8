b88c0a2ab8ffde29825c1ff9b92149853c879497
techstack-io <dcollins31@gmail.com>
2026-09-01 14:40:55 -0500

    Add Pollfish evidence page component

---
+++ client/app/dashboard/evidence/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "@/components/Header";
+import PollfishEvidencePage from "@/components/evidence/PollfishEvidencePage";
+
+export default function PollfishPage() {
+  return (
+    <>
+      <Header />
+      <PollfishEvidencePage />
+    </>
+  );
+}
